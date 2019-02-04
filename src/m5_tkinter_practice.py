481e248c9ed9bdb4ffcd0291778c6c816a0f36ed
hasegaj <hasegaj@rose-hulman.edu>
2019-02-04 18:27:06 -0500

    ?

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Josiah Hasegawa.
+"""  # DO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -13,37 +13,44 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DO: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    root = tkinter.Tk()
 
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DO: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
+    # ----------------------------------    ---------------------------------------
+    some_frame = ttk.Frame(root, padding=25)
+    some_frame.grid()
     # -------------------------------------------------------------------------
-
-    # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DO: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
-
+    forward_button = ttk.Button(some_frame, text='print that thingy')
+    forward_button.grid()
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DO: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
-
+    forward_button['command'] = lambda: print('hello')
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DO: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
-
+    entry_thingy = ttk.Entry(some_frame)
+    entry_thingy.grid()
+    print_entry = ttk.Button(some_frame, text='type something')
+    print_entry['command'] = lambda: whale(entry_thingy)
+    print_entry.grid()
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # DO: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -65,12 +72,29 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
-
+    another_entry = ttk.Entry(some_frame)
+    another_entry.grid()
+    print_entry_again = ttk.Button(some_frame, text='input an integer')
+    print_entry_again['command'] = lambda: derp(entry_thingy, another_entry)
+    print_entry_again.grid()
     # -------------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DO: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
 
+    root.mainloop()
 
+def whale(dah):
+    contents_of_dah = dah.get()
+    if contents_of_dah == 'ok':
+        print('Hello')
+    if contents_of_dah != 'ok':
+        print('Goodbye')
+def derp(string, n):
+    no = string.get()
+    g = n.get()
+    s = int(g)
+    for k in range(s):
+        print(no)
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
